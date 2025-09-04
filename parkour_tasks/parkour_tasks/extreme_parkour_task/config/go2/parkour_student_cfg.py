ad88f45d9c6b3c337b55c089669a1693e4a3d73b
Darth Coder <108871750+CAI23sbP@users.noreply.github.com>
2025-09-04 11:51:46 +0900

    Update parkour_student_cfg.py

--- parkour_tasks/parkour_tasks/extreme_parkour_task/config/go2/parkour_student_cfg.py
+++ parkour_tasks/parkour_tasks/extreme_parkour_task/config/go2/parkour_student_cfg.py
@@ -3,7 +3,7 @@ from isaaclab.utils import configclass
 # Pre-defined configs
 ##
 from parkour_isaaclab.terrains.extreme_parkour.extreme_parkour_terrains_cfg import ExtremeParkourRoughTerrainCfg
-  # isort: skip
+# isort: skip
 from parkour_isaaclab.envs import ParkourManagerBasedRLEnvCfg
 from .parkour_mdp_cfg import * 
 from parkour_tasks.default_cfg import  CAMERA_USD_CFG, CAMERA_CFG, VIEWER
@@ -18,36 +18,26 @@ class ParkourStudentSceneCfg(ParkourTeacherSceneCfg):
         self.terrain.terrain_generator.num_rows = 10
         self.terrain.terrain_generator.num_cols = 20
         self.terrain.terrain_generator.horizontal_scale = 0.1
-        
         for key, sub_terrain in self.terrain.terrain_generator.sub_terrains.items():
             sub_terrain: ExtremeParkourRoughTerrainCfg
             sub_terrain.use_simplified = True 
-            if key == 'demo':
+            sub_terrain.horizontal_scale = 0.1
+            if key == 'parkour_demo':
                 sub_terrain.proportion = 0.15
-                sub_terrain.y_range = (-0.1, 0.1)
-
-            elif key =='parkour_step':
-                sub_terrain.proportion = 0.2
-                sub_terrain.y_range = (-0.1, 0.1)
 
             elif key =='parkour_flat':
                 sub_terrain.proportion = 0.05
-                sub_terrain.y_range = (-0.1, 0.1)
 
-            elif key =='parkour_gap':
+            else:
                 sub_terrain.proportion = 0.2
-                sub_terrain.y_range = (-0.1, 0.1)
+                if key is not 'parkour':
+                    sub_terrain.y_range = (-0.1, 0.1)
 
-            elif key =='parkour_hurdle':
-                sub_terrain.proportion = 0.2
-                sub_terrain.y_range = (-0.1, 0.1)
-            sub_terrain.horizontal_scale = 0.1
 
 
 @configclass
 class UnitreeGo2StudentParkourEnvCfg(ParkourManagerBasedRLEnvCfg):
-    viewer = VIEWER 
-    scene: ParkourStudentSceneCfg = ParkourStudentSceneCfg(num_envs=77, env_spacing=1.)
+    scene: ParkourStudentSceneCfg = ParkourStudentSceneCfg(num_envs=192, env_spacing=1.)
     # Basic settings
     observations: StudentObservationsCfg = StudentObservationsCfg()
     actions: ActionsCfg = ActionsCfg()
@@ -76,34 +66,65 @@ class UnitreeGo2StudentParkourEnvCfg(ParkourManagerBasedRLEnvCfg):
         self.actions.joint_pos.use_delay = True
         self.actions.joint_pos.history_length = 8
 
+
+
 @configclass
-class UnitreeGo2StudentParkourEnvCfg_PLAY(UnitreeGo2StudentParkourEnvCfg):
+class UnitreeGo2StudentParkourEnvCfg_EVAL(UnitreeGo2StudentParkourEnvCfg):
+    viewer = VIEWER 
+    rewards: TeacherRewardsCfg = TeacherRewardsCfg()
     def __post_init__(self):
         # post init of parent
         super().__post_init__()
+        self.scene.num_envs = 256
+        self.episode_length_s = 20.
+        self.commands.base_velocity.debug_vis = True
+
         self.scene.depth_camera_usd = CAMERA_USD_CFG
-        # self.parkours.base_parkour.debug_vis = True
-        # self.commands.base_velocity.debug_vis = True
-        self.scene.num_envs = 1
-        # self.scene.terrain.max_init_terrain_level = None
+        self.scene.terrain.max_init_terrain_level = None
+
+        self.observations.depth_camera.depth_cam.params['debug_vis'] = True
+        self.observations.depth_camera.depth_cam.params['debug_vis'] = True
+
+        self.commands.base_velocity.resampling_time_range = (60.,60.)
+        self.commands.base_velocity.debug_vis = True
+
         if self.scene.terrain.terrain_generator is not None:
-            self.scene.terrain.terrain_generator.num_rows = 2
-            self.scene.terrain.terrain_generator.num_cols = 2
-            self.scene.terrain.terrain_generator.random_difficulty = True
+            self.scene.terrain.terrain_generator.num_rows = 5
+            self.scene.terrain.terrain_generator.num_cols = 5
             self.scene.terrain.terrain_generator.difficulty_range = (0.7,1.0)
             self.scene.terrain.terrain_generator.curriculum = False
-        self.observations.policy.enable_corruption = False
         self.events.randomize_rigid_body_com = None
         self.events.randomize_rigid_body_mass = None
-        self.events.physics_material = None
-        self.events.push_by_setting_velocity = None
-        self.events.random_camera_position.params['rot_noise_range'] = {'pitch':(0, 0)}
-        self.commands.base_velocity.resampling_time_range = (60.,60.)
-        self.episode_length_s = 60.
-
+        self.events.push_by_setting_velocity.interval_range_s = (6.,6.)
+        self.events.random_camera_position.params['rot_noise_range'] = {'pitch':(0, 1)}
+        
         for key, sub_terrain in self.scene.terrain.terrain_generator.sub_terrains.items():
             if key =='parkour_flat':
                 sub_terrain.proportion = 0.0
             else:
-                sub_terrain.proportion = 0.2
+                sub_terrain.proportion = 0.25
                 sub_terrain.noise_range = (0.02, 0.02)
+
+@configclass
+class UnitreeGo2StudentParkourEnvCfg_PLAY(UnitreeGo2StudentParkourEnvCfg):
+    viewer = VIEWER 
+
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+
+        self.scene.num_envs = 16
+        self.episode_length_s = 60.
+
+        if self.scene.terrain.terrain_generator is not None:
+            # self.scene.terrain.terrain_generator.random_difficulty = True
+            self.scene.terrain.terrain_generator.difficulty_range = (0.7,1.0)
+            self.scene.terrain.terrain_generator.curriculum = False
+        self.events.push_by_setting_velocity = None
+        for key, sub_terrain in self.scene.terrain.terrain_generator.sub_terrains.items():
+            if key =='parkour_flat':
+                sub_terrain.proportion = 0.0
+            else:
+                sub_terrain.proportion = 0.25
+                sub_terrain.noise_range = (0.02, 0.02)
+
